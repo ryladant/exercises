a7fb649df17599a270ac268af439610bab94761b
ryladant <ryladant@gmail.com>
2025-01-18 15:44:29 -0300

    Adicionado uma visualização dos eixos usando matplotlib

--- exer0012.py
+++ exer0012.py
@@ -1,9 +1,10 @@
 #rotacionando um vetor (x, y) em theta graus.
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 vetor = np.array([2, 2])
-theta = np.pi / 2
+theta = np.pi/4
 
 matrix_rot = np.array([
     [np.cos(theta), -np.sin(theta)],
@@ -14,3 +15,23 @@ vetor_rot = np.dot(matrix_rot, vetor)
 
 print(f"Vetor original: {vetor}")
 print(f"Vetor Rotacionado: {vetor_rot}")
+
+plt.figure()
+
+#vetor original
+plt.quiver(0, 0, vetor[0], vetor[1], angles='xy', scale_units='xy', scale=1, color='blue', label='Original')
+
+#vetor rotacionado
+plt.quiver(0,0,vetor_rot[0], vetor_rot[1], angles='xy', scale_units='xy', scale=1, color='green', label='rotacionado')
+
+# Configurações do gráfico
+plt.xlim(-3, 3)
+plt.ylim(-3, 3)
+plt.grid()
+plt.legend()
+plt.title('Vetor Original e Rotacionado')
+plt.xlabel('X')
+plt.ylabel('Y')
+
+# Exibe o gráfico
+plt.show()
