ff28d53bf6fe7064ebe2dc623c302206b4c4c978
ryladant <ryladant@gmail.com>
2025-01-18 21:41:29 -0300

    criando funções para pegar os valores e outra para plotar o grafico

---
+++ exer0013.py
@@ -0,0 +1,26 @@
+# Exemplo simples plotando um vetor
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def set_vector():
+    print('Digite o x do vetor')
+    x = int(input())
+
+    print('Digite o y do vetor')
+    y = int(input())
+
+    draw_graph(x, y)
+
+def draw_graph(x, y):
+    plt.quiver(0,0,x,y,angles='xy',scale_units='xy',scale=1,color='red')
+
+    plt.ylim(-y-2,y+2)
+    plt.xlim(-x-2,x+2)
+
+    plt.legend(loc='lower left', title='Meu vetor')
+    plt.grid()
+    plt.title(f'Grafico do vetor ({x}, {y})')
+    plt.show()
+
+set_vector()
